5cb87e7abc1bef703e7a9465afc434971209af5c
ValentinGallo <51474574+ValentinGallo@users.noreply.github.com>
2021-12-13 09:10:29 +0100

    Update Rank.vue

--- src/components/home/Rank.vue
+++ src/components/home/Rank.vue
@@ -1,27 +1,97 @@
 <template>
-<Card>
-    <template #title>
-        Classement
-    </template>
+  <Card class="m-3 border-2 border-blue-400">
+    <template #title> Classement </template>
     <template #content>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
-        quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+      <DataTable :value="cities" sortField="rating" :sortOrder="-1" responsiveLayout="scroll">
+        <Column field="name" header="Name"></Column>
+        <Column field="description" header="Description"></Column>
+        <Column field="rating" header="Rating" :sortable="true"></Column>
+      </DataTable>
     </template>
-</Card>
+  </Card>
 </template>
 
 <script>
-import Card from 'primevue/card';
+import Card from "primevue/card";
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 
 export default {
   name: "SearchBar",
   components: {
     Card,
+    DataTable,
+    Column
+  },
+  data() {
+    return {
+      cities: [
+        {
+          id: "1000",
+          name: "Paris",
+          description: "Product Description",
+          rating: 5,
+        },
+        {
+          id: "1001",
+          name: "Black Watch",
+          description: "Product Description",
+          rating: 4,
+        },
+        {
+          id: "1002",
+          name: "Blue Band",
+          description: "Product Description",
+          rating: 3,
+        },
+        {
+          id: "1003",
+          name: "Blue T-Shirt",
+          description: "Product Description",
+          rating: 5,
+        },
+        {
+          id: "1004",
+          name: "Bracelet",
+          description: "Product Description",
+          rating: 4,
+        },
+        {
+          id: "1005",
+          name: "Brown Purse",
+          description: "Product Description",
+          rating: 4,
+        },
+        {
+          id: "1006",
+          name: "Chakra Bracelet",
+          description: "Product Description",
+          rating: 3,
+        },
+        {
+          id: "1007",
+          name: "Galaxy Earrings",
+          description: "Product Description",
+          rating: 5,
+        },
+        {
+          id: "1008",
+          name: "Game Controller",
+          description: "Product Description",
+          rating: 4,
+        },
+        {
+          id: "1009",
+          name: "Gaming Set",
+          description: "Product Description",
+          rating: 3,
+        },
+      ],
+    };
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
