314b46b5b7047ebc25f31ba5054b05a49f378f17
ValentinGallo <51474574+ValentinGallo@users.noreply.github.com>
2022-01-10 15:37:31 +0100

    Podium responsive

--- src/components/home/rank/Podium.vue
+++ src/components/home/rank/Podium.vue
@@ -5,25 +5,28 @@
       :to="{ name: 'city', params: { name: cities[0].name } }"
       class="no-underline text-dark"
     >
-      <span class="top-1"
-        >{{ cities[0].name }}<img :src="cities[0].flag" class="top-flag"
-      /></span>
+      <span class="absolute top-1">
+        {{ cities[0].name }}
+        <img :src="cities[0].flag" class="top-flag" />
+      </span>
     </router-link>
     <router-link
       :to="{ name: 'city', params: { name: cities[1].name } }"
       class="no-underline text-dark"
     >
-      <span class="top-2"
-        >{{ cities[1].name }}<img :src="cities[1].flag" class="top-flag"
-      /></span>
+      <span class="absolute top-2">
+        {{ cities[1].name }}
+        <img :src="cities[1].flag" class="top-flag" />
+      </span>
     </router-link>
     <router-link
       :to="{ name: 'city', params: { name: cities[2].name } }"
       class="no-underline text-dark"
     >
-      <span class="top-3"
-        >{{ cities[2].name }} <img :src="cities[2].flag" class="top-flag"
-      /></span>
+      <span class="absolute top-3">
+        {{ cities[2].name }}
+        <img :src="cities[2].flag" class="top-flag" />
+      </span>
     </router-link>
   </div>
 </template>
@@ -41,38 +44,42 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .container {
-  width: 500px;
+  max-width: 500px;
   margin: auto;
   position: relative;
 }
 
 span {
-  font-size: 1.5rem;
+  font-size: 1.5em;
   font-weight: 800;
-  width: 167px;
+  width: 33%;
   text-align: center;
 }
 
+@media screen and (max-width: 500px) {
+  span {
+    font-size: 1.2em;
+  }
+}
+
 .top-1 {
-  position: absolute;
-  left: 168px;
+  left: 34%;
 }
 
 .top-2 {
-  position: absolute;
   left: 0px;
-  top: 49px;
+  top: 20%;
 }
 
 .top-3 {
-  position: absolute;
-  left: 333px;
-  top: 80px;
+  left: 67%;
+  top: 35%;
 }
+
 .top-flag {
   position: absolute;
-  top: -15px;
-  right: 70px;
   height: 1rem;
+  top: -40%;
+  left: 43%;
 }
 </style>
