cfb3d6be34d69085ef8bddfc8ffff72933f89e57
ValentinGallo <51474574+ValentinGallo@users.noreply.github.com>
2022-02-11 11:41:18 +0100

    New navbar

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -1,32 +1,33 @@
 <template>
-  <Menubar>
-    <template #start><router-link to="/" class="no-underline font-bold"><Button label="SafeTravel" icon="pi pi-briefcase" class="p-button-secondary p-button-text" /></router-link></template>
-    <template #end>
-      <div v-if="isLogged">
+  <div class="navbar flex justify-content-between">
+    <router-link to="/" class="no-underline font-bold"
+      ><Button
+        label="SafeTravel"
+        icon="pi pi-briefcase"
+        class="p-button-secondary p-button-text"
+    /></router-link>
+    <div v-if="isLogged">
       <Button type="button" label="Yoann" @click="toggle" />
-      <Menu ref="menu" :model="items[1].items" :popup="true" />
-      </div>
-      <div v-else>
-        <router-link to="/connexion" class="no-underline mr-2">
-          <Button type="button" label="connexion" />
-        </router-link>
-        <router-link to="/inscription" class="no-underline">
-          <Button type="button" label="inscription" />
-        </router-link>
-      </div>
-    </template>
-  </Menubar>
+      <Menu ref="menu" :model="items" :popup="true" />
+    </div>
+    <div v-else>
+      <router-link to="/connexion" class="no-underline mr-2">
+        <Button type="button" label="connexion" />
+      </router-link>
+      <router-link to="/inscription" class="no-underline">
+        <Button type="button" label="inscription" />
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
-import Menubar from "primevue/menubar";
 import Menu from "primevue/menu";
 import Button from "primevue/button";
 
 export default {
   name: "NavBar",
   components: {
-    Menubar,
     Menu,
     Button,
   },
@@ -34,34 +35,22 @@ export default {
     return {
       items: [
         {
-          label: "SafeTravel",
-          icon: "pi pi-briefcase",
-          class: "text-primary",
-          to: "/",
+          label: "Profil",
+          icon: "pi pi-user-edit",
+          to: "/profil",
         },
         {
-          label: "Yoann",
-          icon: "pi pi-user",
-          items: [
-            {
-              label: "Profil",
-              icon: "pi pi-user-edit",
-              to: "/profil",
-            },
-            {
-              label: "Mes voyages",
-              icon: "pi pi-car",
-              to: "/travels",
-            },
-            {
-              separator: true,
-            },
-            {
-              label: "Deconnexion",
-              icon: "pi pi-sign-out",
-              to: "/logout",
-            },
-          ],
+          label: "Mes voyages",
+          icon: "pi pi-car",
+          to: "/travels",
+        },
+        {
+          separator: true,
+        },
+        {
+          label: "Deconnexion",
+          icon: "pi pi-sign-out",
+          to: "/logout",
         },
       ],
     };
@@ -73,7 +62,7 @@ export default {
   },
   computed: {
     isLogged() {
-      return this.$store.state.isLogged
+      return this.$store.state.isLogged;
     },
   },
 };
@@ -81,4 +70,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.navbar{
+  background-color: #f5f5f5;
+  padding: 0.5rem;
+}
 </style>
