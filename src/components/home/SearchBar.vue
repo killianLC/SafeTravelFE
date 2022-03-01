34e4fc2d2e66a15738cc3d5d3915091df058160e
ValentinGallo <51474574+ValentinGallo@users.noreply.github.com>
2022-03-01 22:52:56 +0100

    Update searchBar

--- src/components/home/SearchBar.vue
+++ src/components/home/SearchBar.vue
@@ -1,38 +1,63 @@
 <template>
   <span class="p-input-icon-left w-full">
     <i class="pi pi-search" />
-    <InputText
+    <AutoComplete
       class="p-inputtext-lg w-full"
-      id="username"
-      type="text"
-      placeholder="Rechercher votre ville"
+      :suggestions="filteredCities"
+      @complete="searchCity($event)"
+      field="name"
       @keyup.enter="search"
       v-model="value"
+      placeholder="Rechercher votre ville"
     />
   </span>
 </template>
 
 <script>
-import InputText from "primevue/inputtext";
+import AutoComplete from "primevue/autocomplete";
+import axios from "axios";
 
 export default {
   name: "SearchBar",
   components: {
-    InputText,
+    AutoComplete,
   },
   data() {
     return {
       value: "",
+      cities: null,
+      filteredCities: null,
     };
   },
   methods: {
     search() {
       this.$router.push({ name: "city", params: { name: this.value } });
     },
+    searchCity(event) {
+      setTimeout(() => {
+        if (!event.query.trim().length) {
+          this.filteredCities = [...this.cities];
+        } else {
+          this.filteredCities = this.cities.filter((city) => {
+            return city.name
+              .toLowerCase()
+              .startsWith(event.query.toLowerCase());
+          });
+        }
+      }, 250);
+    },
+  },
+  created() {
+    axios
+      .get("http://localhost:8080/cities")
+      .then((response) => (this.cities = response.data));
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.p-input-icon-left :deep(.p-autocomplete-input) {
+  width: 100%;
+}
 </style>
